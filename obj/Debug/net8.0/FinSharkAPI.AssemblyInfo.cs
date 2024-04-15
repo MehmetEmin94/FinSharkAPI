71c3ec6b2a8dcf40907be476c4926ff6ce96f2e0
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-04-15 14:55:16 +0300

    extension packs added

--- obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fbe2197ef7d64f1b526eff9fc3a4e3af283a923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd0de23b3be5705452ca65de5773fc81a1730b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
