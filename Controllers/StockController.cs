8b58b964495b46c52a73e175993b22f3a8c33121
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-04-16 13:39:24 +0300

    test stock controller

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -2,11 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinSharkAPI.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinSharkAPI.Controllers
 {
-    public class StockController
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController:ControllerBase
     {
-        
+        private readonly AppDbContext _dbContext;
+        public StockController(AppDbContext dbContext)
+        {
+            _dbContext=dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetStocks()
+        {
+            var stocks = _dbContext.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleStock([FromRoute] int id)
+        {
+            var stock = _dbContext.Stocks.Find(id);
+            if(stock is null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
     }
 }
