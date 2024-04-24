9b6682104475fb2c8be43a5338fac4c9ff0f9760
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-04-24 13:48:57 +0300

    sorting and pagination

--- obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faac8c563b13386202226958b3f5bd74b886ddac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c76ec3cfa8006a4a879637c2d59fdf2d56205d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
