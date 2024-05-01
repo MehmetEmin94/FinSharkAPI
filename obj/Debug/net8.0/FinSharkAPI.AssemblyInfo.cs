59b1293cf16f0236c96b0c26dacae8d9a8f28370
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-05-01 13:51:12 +0300

    login

--- obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinSharkAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045172ea4508c327455af1485ff3ae1ac44030c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbb59027381f0500130552ec898bb9716ffe4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinSharkAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
